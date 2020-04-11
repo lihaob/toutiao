295849e58c0b28f99f915bd848ddd773807ad1bd
LiHbeen <51393642+LiHbeen@users.noreply.github.com>
2020-04-11 20:18:55 +0800

    基本完成列表页尚未实现下拉加载

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,11 +8,22 @@
 			@click="changeTab(index)">{{item.name}}</view>
 		</scroll-view>
 		<!-- 内容块滑动 -->
-		
-		<block v-for="(item1,index1) in demo">
-			<common-list :item="item1"></common-list>
-			<divider></divider>
-		</block>
+		<swiper :duration="150" :current="tabIndex" @change="onChangeTab"
+		:style="'height:'+scrollH+'px;'">
+			<swiper-item v-for="(item1,index1) in tabBars" :key="index1">
+				<scroll-view scroll-y="true" :style="'height:'+scrollH+'px;'">
+					<template v-if="news_list.return_count>0">
+						<block v-for="(item2,index2) in news_list.data" :key="index2">
+							<common-list :item="item2" :index="index2"></common-list>
+							<divider></divider>
+						</block>
+					</template>
+					<template v-else>
+						<no-thing></no-thing>
+					</template>
+				</scroll-view>
+			</swiper-item>
+		</swiper>
 		
 
 	</view>
@@ -21,10 +32,12 @@
 <script>
 	import commonList from '@/components/custom/common-list/common-list.vue'
 	import divider from '@/components/custom/divider/divider.vue'
+	import noThing from '@/components/custom/no-thing/no-thing.vue'
 	export default {
 		components:{
 			commonList,
-			divider
+			divider,
+			noThing
 		},
 		data() {
 			return {
@@ -35,219 +48,73 @@
 				tabIndex:0,
 				//顶部选项卡
 				tabBars:[{
-					name:"关注"
+					name:"热点",
+					type:"news_hot"
 				},{
-					name:"推荐"
+					name:"社会",
+					type:"news_society"
 				},{
-					name:"热点"
+					name:"娱乐",
+					type:"news_entertainment"
 				},{
-					name:"财经"
+					name:"科技",
+					type:"news_tech"
 				},{
-					name:"娱乐"
+					name:"军事",
+					type:"news_military"
 				},{
-					name:"军事"
+					name:"体育",
+					type:"news_sports"
 				},{
-					name:"历史"
+					name:"汽车",
+					type:"news_car"
 				},{
-					name:"本地"
+					name:"财经",
+					type:"news_finance"
 				},{
-					name:"体育"
+					name:"国际",
+					type:"news_world"
+				},{
+					name:"时尚",
+					type:"news_fashion"
+				},{
+					name:"旅游",
+					type:"news_travel"
+				},{
+					name:"探索",
+					type:"news_discovery"
+				},{
+					name:"育儿",
+					type:"news_baby"
+				},{
+					name:"养生",
+					type:"news_regimen"
+				},{
+					name:"故事",
+					type:"news_story"
+				},{
+					name:"美文",
+					type:"news_essay"
+				},{
+					name:"游戏",
+					type:"news_game"
+				},{
+					name:"历史",
+					type:"news_history"
+				},{
+					name:"美食",
+					type:"news_food"
 				}],
-					demo: [{
-						"media_name": "懒熊体育",
-						"ban_comment": 0,
-						"abstract": "截至北京时间4月11日5点40分，全球新冠肺炎累计确诊人数为1594315，加中国确诊人数在内，全球累计确诊将近170万，其中美国确诊人数超过49万，法国、德国确诊人数超过12万。",
-						"image_list": [
-							 
-						],
-						"datetime": "2020-04-11 09:42",
-						"article_type": 0,
-						"tag": "news_sports",
-						"has_m3u8_video": 0,
-						"display_dt": 1586557084,
-						"has_mp4_video": 0,
-						"aggr_type": 1,
-						"cell_type": 0,
-						"article_sub_type": 0,
-						"bury_count": 0,
-						"title": "NBA无限期暂停球员交易 日本一篮球队大规模感染丨全球体育疫情早报",
-						"source_icon_style": 1,
-						"tip": 0,
-						"has_video": false,
-						"share_url": "http://toutiao.com/group/6814210793093988872/?app=news_article&is_hit_share_recommend=0",
-						"source": "懒熊体育",
-						"comment_count": 9,
-						"article_url": "http://www.lanxiongsports.com/?c=posts&a=view&id=18639",
-						"large_mode": true,
-						"large_image_url": "http://p29-tt.byteimg.com/img/pgc-image/RvnVEqIEKpBCaJ~tplv-tt-cs0:640:360.jpg",
-						"publish_time": 1586557084,
-						"group_flags": 0,
-						"gallary_image_count": 3,
-						"action_extra": "{\"channel_id\": 3189398957}",
-						"tag_id": "6814210793093988872",
-						"source_url": "/i6814210793093988872/",
-						"display_url": "http://toutiao.com/group/6814210793093988872/",
-						"is_stick": false,
-						"item_id": "6814210793093988872",
-						"repin_count": 88,
-						"cell_flag": 262155,
-						"source_open_url": "sslocal://profile?uid=3405367810",
-						"level": 0,
-						"digg_count": 107,
-						"behot_time": 1586569350,
-						"hot": 0,
-						"cursor": 1586569350999,
-						"url": "http://www.lanxiongsports.com/?c=posts&a=view&id=18639",
-						"user_repin": 0,
-						"has_image": true,
-						"video_style": 0,
-						"media_info": {
-						"avatar_url": "http://p8.pstatp.com/large/dc0f0000d70e1113f6e1",
-						"media_id": 3405401991,
-						"name": "懒熊体育",
-						"user_verified": true
-						},
-						"group_id": "6814210793093988872"
-					},
-					{
-						"media_name": "懒熊体育",
-						"ban_comment": 0,
-						"abstract": "截至北京时间4月11日5点40分，全球新冠肺炎累计确诊人数为1594315，加中国确诊人数在内，全球累计确诊将近170万，其中美国确诊人数超过49万，法国、德国确诊人数超过12万。",
-						"image_list": [
-							{
-							"url": "https://p3.pstatp.com/list/pgc-image/7ba835f4aa844f3790a3eb7cac2ea0ad",
-							"width": 640,
-							"height": 360
-							}
-						],
-						"datetime": "2020-04-11 09:42",
-						"article_type": 0,
-						"tag": "news_sports",
-						"has_m3u8_video": 0,
-						"display_dt": 1586557084,
-						"has_mp4_video": 0,
-						"aggr_type": 1,
-						"cell_type": 0,
-						"article_sub_type": 0,
-						"bury_count": 0,
-						"title": "NBA无限期暂停球员交易 日本一篮球队大规模感染丨全球体育疫情早报",
-						"source_icon_style": 1,
-						"tip": 0,
-						"has_video": true,
-						"share_url": "http://toutiao.com/group/6814210793093988872/?app=news_article&is_hit_share_recommend=0",
-						"source": "懒熊体育",
-						"comment_count": 9,
-						"article_url": "http://www.lanxiongsports.com/?c=posts&a=view&id=18639",
-						"large_mode": true,
-						"large_image_url": "http://p29-tt.byteimg.com/img/pgc-image/RvnVEqIEKpBCaJ~tplv-tt-cs0:640:360.jpg",
-						"publish_time": 1586557084,
-						"group_flags": 0,
-						"gallary_image_count": 3,
-						"action_extra": "{\"channel_id\": 3189398957}",
-						"tag_id": "6814210793093988872",
-						"source_url": "/i6814210793093988872/",
-						"display_url": "http://toutiao.com/group/6814210793093988872/",
-						"is_stick": false,
-						"item_id": "6814210793093988872",
-						"repin_count": 88,
-						"cell_flag": 262155,
-						"source_open_url": "sslocal://profile?uid=3405367810",
-						"level": 0,
-						"digg_count": 107,
-						"behot_time": 1586569350,
-						"hot": 0,
-						"cursor": 1586569350999,
-						"url": "http://www.lanxiongsports.com/?c=posts&a=view&id=18639",
-						"user_repin": 0,
-						"has_image": true,
-						"video_style": 0,
-						"media_info": {
-						"avatar_url": "http://p8.pstatp.com/large/dc0f0000d70e1113f6e1",
-						"media_id": 3405401991,
-						"name": "懒熊体育",
-						"user_verified": true
-						},
-						"group_id": "6814210793093988872"
-					},
-					{
-						"media_name": "懒熊体育",
-						"ban_comment": 0,
-						"abstract": "截至北京时间4月11日5点40分，全球新冠肺炎累计确诊人数为1594315，加中国确诊人数在内，全球累计确诊将近170万，其中美国确诊人数超过49万，法国、德国确诊人数超过12万。",
-						"image_list": [
-							{
-							"url": "https://p3.pstatp.com/list/pgc-image/7ba835f4aa844f3790a3eb7cac2ea0ad",
-							"width": 640,
-							"height": 360
-							},
-							{
-							"url": "https://p3.pstatp.com/list/pgc-image/7ba835f4aa844f3790a3eb7cac2ea0ad",
-							"width": 640,
-							"height": 360
-							},
-							{
-							"url": "https://p3.pstatp.com/list/pgc-image/7ba835f4aa844f3790a3eb7cac2ea0ad",
-							"width": 640,
-							"height": 360
-							}
-						],
-						"datetime": "2020-04-11 09:42",
-						"article_type": 0,
-						"tag": "news_sports",
-						"has_m3u8_video": 0,
-						"display_dt": 1586557084,
-						"has_mp4_video": 0,
-						"aggr_type": 1,
-						"cell_type": 0,
-						"article_sub_type": 0,
-						"bury_count": 0,
-						"title": "NBA无限期暂停球员交易 日本一篮球队大规模感染丨全球体育疫情早报",
-						"source_icon_style": 1,
-						"tip": 0,
-						"has_video": false,
-						"share_url": "http://toutiao.com/group/6814210793093988872/?app=news_article&is_hit_share_recommend=0",
-						"source": "懒熊体育",
-						"comment_count": 9,
-						"article_url": "http://www.lanxiongsports.com/?c=posts&a=view&id=18639",
-						"large_mode": true,
-						"large_image_url": "http://p29-tt.byteimg.com/img/pgc-image/RvnVEqIEKpBCaJ~tplv-tt-cs0:640:360.jpg",
-						"publish_time": 1586557084,
-						"group_flags": 0,
-						"gallary_image_count": 3,
-						"action_extra": "{\"channel_id\": 3189398957}",
-						"tag_id": "6814210793093988872",
-						"source_url": "/i6814210793093988872/",
-						"display_url": "http://toutiao.com/group/6814210793093988872/",
-						"is_stick": false,
-						"item_id": "6814210793093988872",
-						"repin_count": 88,
-						"cell_flag": 262155,
-						"source_open_url": "sslocal://profile?uid=3405367810",
-						"level": 0,
-						"digg_count": 107,
-						"behot_time": 1586569350,
-						"hot": 0,
-						"cursor": 1586569350999,
-						"url": "http://www.lanxiongsports.com/?c=posts&a=view&id=18639",
-						"user_repin": 0,
-						"has_image": true,
-						"video_style": 0,
-						"media_info": {
-						"avatar_url": "http://p8.pstatp.com/large/dc0f0000d70e1113f6e1",
-						"media_id": 3405401991,
-						"name": "懒熊体育",
-						"user_verified": true
-						},
-						"group_id": "6814210793093988872"
-					}]
+				news_list: []
 			}
 		},
 		onLoad() {
 			var res=uni.getSystemInfo({
 				success:res=>{
 					this.scrollH=res.windowHeight-uni.upx2px(100);
-					console.log(this.scrollH)
 				}
 			})
+			this.getData();
 		},
 		onNavigationBarSearchInputClicked(){
 			uni.navigateTo({
@@ -255,12 +122,32 @@
 			})
 		},
 		methods: {
+			onChangeTab(e){
+				this.changeTab(e.detail.current);
+			},
 			changeTab(index){
 				if (this.tabIndex === index){
 					return ;
 				}
 				this.tabIndex = index;
 				this.scrollInto = 'tab' + index;
+				this.getData();
+			},
+			getData(){
+/* 				console.log("https://m.toutiao.com/list/?ac=wap&format=json_raw&tag="+this.tabBars[this.tabIndex].type+"&as=A175ADC1C1BAE7E&cp=5D118A3E877E8E1&_signature=f60bLAAAIr6R60RntmQLRn-tGz")
+ */				uni.request({
+					url:"https://m.toutiao.com/list/?ac=wap&format=json_raw&tag="+this.tabBars[this.tabIndex].type+"&as=A175ADC1C1BAE7E&cp=5D118A3E877E8E1&_signature=f60bLAAAIr6R60RntmQLRn-tGz",
+					success: (res) => {
+						console.log("success")
+						this.news_list=res.data;
+					},
+					fail: ()=>{
+						console.log("error")
+					},
+					complete: ()=>{
+						console.log("completed");
+					}
+				})
 			}
 		}
 	}
